df271369e0462e2c0849a51a8005d3061055a49e
Estu <estuardovilleda2001@gmail.com>
2023-10-21 11:24:50 -0600

    feat(backend): add morgan and cors middlewares

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,18 @@
 import express from 'express';
 import dotenv from 'dotenv';
-
 dotenv.config();
+
+import cors from 'cors';
+import morgan from 'morgan';
 
 import products from './data/products.js';
 
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+app.use(cors());
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
