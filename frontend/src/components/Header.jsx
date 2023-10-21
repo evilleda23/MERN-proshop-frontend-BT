988b0bd226e988f0d999d137f8cd922cf421119f
Estu <estuardovilleda2001@gmail.com>
2023-10-21 09:03:15 -0600

    feat: basic routing

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
+
+import { LinkContainer } from 'react-router-bootstrap';
+
 import logo from '../assets/logo.png';
 function Header() {
   return (
@@ -12,22 +15,28 @@ function Header() {
         collapseOnSelect
       >
         <Container>
-          <Navbar.Brand href='/'>
-            <img
-              src={logo}
-              alt=''
-            />
-            ProShop
-          </Navbar.Brand>
+          <LinkContainer to='/'>
+            <Navbar.Brand>
+              <img
+                src={logo}
+                alt=''
+              />
+              ProShop
+            </Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ms-auto'>
-              <Nav.Link href='/cart'>
-                <FaShoppingCart /> Cart
-              </Nav.Link>
-              <Nav.Link href='/login'>
-                <FaUser /> Sign In
-              </Nav.Link>
+              <LinkContainer to='/cart'>
+                <Nav.Link>
+                  <FaShoppingCart /> Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/login'>
+                <Nav.Link>
+                  <FaUser /> Sign In
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
