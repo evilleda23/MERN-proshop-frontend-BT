7e6a01aa4b225f58dad65a37232f38a2508c2971
Estu <estuardovilleda2001@gmail.com>
2023-10-21 11:34:11 -0600

    feat(front): fetch products from backend

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,10 +1,20 @@
+import { useEffect, useState } from 'react';
 import { Row, Col } from 'react-bootstrap';
-
 import Product from '../components/Product';
 
-import products from '../products';
+import { get } from '../utils/http-request';
+import enviroment from '../environments';
 
 function HomeScreen() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const data = await get(enviroment.getProductsUrl);
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
   return (
     <>
       <h1>Latest Products</h1>
