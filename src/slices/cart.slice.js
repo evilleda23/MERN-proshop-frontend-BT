059d8a6f5941244d7a25f725c8550c32af91440f
Estu <estuardovilleda2001@gmail.com>
2023-10-24 15:42:04 -0600

    chore: create cart util

--- src/slices/cart.slice.js
+++ src/slices/cart.slice.js
@@ -1,12 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { updateCart } from '../utils/cart.util';
 
 const initialState = localStorage.getItem('cart')
   ? JSON.parse(localStorage.getItem('cart'))
   : { cartItems: [] };
-
-const addDecimals = (num) => {
-  return Math.round((num * 100) / 100).toFixed(2);
-};
 
 const cartSlice = createSlice({
   name: 'cart',
@@ -26,27 +23,7 @@ const cartSlice = createSlice({
       } else {
         state.cartItems = [...state.cartItems, item];
       }
-
-      //Calculate items price
-      state.itemsPrice = addDecimals(
-        state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
-      );
-      //shipping (over $100 free shipping, else $10)
-      state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
-
-      //tax price (15%)
-      state.taxPrice = addDecimals(
-        Number((0.15 * state.itemsPrice).toFixed(2))
-      );
-
-      //total price
-      state.totalPrice = (
-        Number(state.itemsPrice) +
-        Number(state.shippingPrice) +
-        Number(state.taxPrice)
-      ).toFixed(2);
-
-      localStorage.setItem('cart', JSON.stringify(state));
+      return updateCart(state);
     },
   },
 });
