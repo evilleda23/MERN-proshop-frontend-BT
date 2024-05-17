8aca5f9c820ca35e684aae95f2496a143956d27f
Estuardo Villeda <evilleda@bdgsa.net>
2024-05-17 09:56:39 -0600

    Revert "Merge pull request #3 from evilleda23/abc"
    This reverts commit fc8d8e4d2fe1dbee925727ea79ae2d0601d3e5a8, reversing
    changes made to 2a6a26927f321c03081346794b5e00b85719252e.

---
+++ src/slices/users.api.slice.js
@@ -0,0 +1,42 @@
+import { USERS_URL } from '../environments/environment';
+
+import { apiSlice } from './api.slice';
+
+export const usersSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    register: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/register`,
+        method: 'POST',
+        body: data,
+      }),
+    }),
+    login: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/auth`,
+        method: 'POST',
+        body: data,
+      }),
+    }),
+    logout: builder.mutation({
+      query: () => ({
+        url: `${USERS_URL}/logout`,
+        method: 'PUT',
+      }),
+    }),
+    profile: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/profile`,
+        method: 'PUT',
+        body: data,
+      }),
+    }),
+  }),
+});
+
+export const {
+  useRegisterMutation,
+  useLoginMutation,
+  useLogoutMutation,
+  useProfileMutation,
+} = usersSlice;
